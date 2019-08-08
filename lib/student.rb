2e4e16175226cb83b21f2c421609455cabba8ef0
David Forer <david.j.forer@gmail.com>
2019-08-08 02:23:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,7 +40,11 @@ class Student
     end
   end
   
-  
+   def update
+    sql = "UPDATE students SET name = ?, grade = ? 
+    WHERE ID = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
   
   
 end
